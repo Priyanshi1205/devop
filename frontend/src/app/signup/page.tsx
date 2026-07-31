4c49465ac58689d10838f4d3d2bfa364ef0e72ed
Priyanshi1205 <64695000+Priyanshi1205@users.noreply.github.com>
2026-07-31 15:12:32 +0530

    Update page.tsx

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -57,7 +57,7 @@ export default function SignupPage() {
     const lastName = names.slice(1).join(' ') || 'User';
 
     try {
-      const res = await fetch('/api/auth/register', {
+     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
